c8150c15fd7e29cee9e3cee8a86bcf33d58523ab
Seid <seidshemsu486@yahoo.com>
2021-08-14 18:39:06 +0300

    Localization done

--- app/src/main/java/com/izhar/crms/MainActivity.java
+++ app/src/main/java/com/izhar/crms/MainActivity.java
@@ -8,9 +8,14 @@ import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.izhar.crms.activities.IncidentReport;
 import com.izhar.crms.activities.MissingPersonReport;
@@ -72,6 +77,51 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1002);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.setting){
+            SharedPreferences lang = getSharedPreferences("language", MODE_PRIVATE);
+            BottomSheetDialog dialog = new BottomSheetDialog(MainActivity.this);
+            dialog.setContentView(R.layout.settings);
+            TextView am, om, en;
+            Button save = dialog.findViewById(R.id.save);
+            am = dialog.findViewById(R.id.am);
+            om = dialog.findViewById(R.id.om);
+            en = dialog.findViewById(R.id.en);
+
+            am.setOnClickListener(v -> {
+                lang.edit().putString("language", "am").apply();
+                am.setBackgroundResource(R.drawable.selected_lang_bg);
+                en.setBackgroundResource(R.drawable.lang_bg);
+                om.setBackgroundResource(R.drawable.lang_bg);
+            });
+            om.setOnClickListener(v -> {
+                lang.edit().putString("language", "om").apply();
+                om.setBackgroundResource(R.drawable.selected_lang_bg);
+                en.setBackgroundResource(R.drawable.lang_bg);
+                am.setBackgroundResource(R.drawable.lang_bg);
+            });
+            en.setOnClickListener(v -> {
+                lang.edit().putString("language", "en").apply();
+                en.setBackgroundResource(R.drawable.selected_lang_bg);
+                am.setBackgroundResource(R.drawable.lang_bg);
+                om.setBackgroundResource(R.drawable.lang_bg);
+            });
+            save.setOnClickListener(v -> {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                finish();
+            });
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 
     private void setLanguage() {
